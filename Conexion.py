5477f8600c7839d9d5554423b7b28e880dfcd954
alex010620 <66653033+alex010620@users.noreply.github.com>
2021-07-14 19:48:10 -0400

    Adz

--- Conexion.py
+++ Conexion.py
@@ -3,6 +3,6 @@ server = 'tarea7y8.database.windows.net'
 database = 'tarea7y8'
 username = 'ADM-YAMC'
 password = "Ya95509550"  
-driver= '{ODBC Driver 17 for SQL Server}'
+driver= '{SQL Server}'
 
 conexion = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
